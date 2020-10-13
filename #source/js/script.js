38dcd73832160c9fd6e9e22361d692ff11dfa0f6
IDVAN3 <53148453+IDVAN3@users.noreply.github.com>
2020-10-13 13:21:21 +0300

    Add files via upload

---
+++ #source/js/script.js
@@ -0,0 +1,59 @@
+'use strict'
+$(document).ready(function () {
+
+    /*ibg*/
+
+    function ibg() {
+        $.each($('.ibg'), function (index, val) {
+            if ($(this).find('img').length > 0) {
+                let src_img = $(this).find('img').attr('src');
+                $(this).css('backgroundImage', 'url("' + src_img + '")');
+            }
+        });
+    }
+
+    ibg();
+
+    /*попап*/
+
+    function openPopup(id) {
+        $(".js-popup[data-id-popup='" + id + "']").fadeIn(300);
+    }
+
+    function close_popup() {
+        $('.js-popup').fadeOut(300);
+    }
+
+    $('.js-popup__close').click(close_popup);
+
+    $('.js-btn-popup').click(function (e) {
+        e.preventDefault();
+        let index_btn_popup = $(this).attr('href');
+        openPopup(index_btn_popup);
+    });
+
+    $('.js-popup').click(function (e) {
+        let popup = $('.js-popup__wrapp');
+        if (!popup.is(e.target) && popup.has(e.target).length === 0)
+            close_popup();
+    });
+
+    $('.header__link').click(function (event) {
+        event = $(this).closest('.header__list').find('.header__link.active');
+        event.removeClass('active');
+        $(this).addClass('active');
+    })
+
+    $('.slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+    });
+
+    $('.header__burger').click(function () {
+        $('.header__burger,.header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+        // $('.header__menu.active').css('top', heightHeader);
+    });
+
+});
